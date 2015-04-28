0b61471564daa1ee7297c3a012b5a3966dddcff1
Tomiblanchard <tomiblanchard@gmail.com>
2015-04-28 11:26:44 +0100

    data binding

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -13,3 +13,31 @@ describe('GitUserSearchController', function() {
   });
 
 });
+
+describe('when searching for a user', function() {
+
+  beforeEach(module('GitUserSearch'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }));
+
+  var items = [
+    {
+      "login": "tansaku",
+      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+      "html_url": "https://github.com/tansaku"
+    },
+    {
+      "login": "stephenlloyd",
+      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+      "html_url": "https://github.com/stephenlloyd"
+    }
+  ];
+
+  it('displays search results', function() {
+    expect(ctrl.searchResult.items).toEqual(items);
+  });
+});
