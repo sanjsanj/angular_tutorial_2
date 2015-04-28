2e9837db851d5a3b0fafd9957f28bbecffccd9d7
Tomiblanchard <tomiblanchard@gmail.com>
2015-04-28 10:57:01 +0100

    installed karma

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    jshint: {
+      all: ['Gruntfile.js', 'src/**/*.js', 'spec/**/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'jasmine']);
+
+};
