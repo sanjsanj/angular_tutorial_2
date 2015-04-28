148cd6c38981a76478c59f662db86123b5376f45
sanjsanj <sanjsanj@hotmail.com>
2015-04-28 14:41:09 +0100

    Can search git using the API

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,22 +1,16 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+
+  var searchResource = $resource('https://api.github.com/search/users?access_token=f44ddb06692b6fab2bf2d1ef692c0da31c1faad2');
+
+  // var searchResource = $resource('https://api.github.com/search/users');
 
   var self = this;
 
   self.doSearch = function(){
-    self.searchResult = {
-      items: [
-        {
-          "login": "tansaku",
-          "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-          "html_url": "https://github.com/tansaku"
-        },
-        {
-          "login": "stephenlloyd",
-          "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-          "html_url": "https://github.com/stephenlloyd"
-        }
-      ]
-    };
+    console.log(self.searchTerm);
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm }
+    );
   };
 
 }]);
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -12,34 +12,36 @@ describe('GitUserSearchController', function() {
     expect(ctrl.searchTerm).toBeUndefined();
   });
 
-});
+  describe('when searching for a user', function() {
 
-describe('when searching for a user', function() {
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+        { items: items }
+      );
+    }));
 
-  beforeEach(module('GitUserSearch'));
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
 
-  var ctrl;
-
-  beforeEach(inject(function($controller) {
-    ctrl = $controller('GitUserSearchController');
-  }));
-
-  var items = [
-    {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-  ];
-
-  it('displays search results', function() {
-    ctrl.searchTerm = 'hello';
-    ctrl.doSearch();
-    expect(ctrl.searchResult.items).toEqual(items);
+    it('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
   });
 });
